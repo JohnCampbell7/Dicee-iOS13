aeb1e115bd9a8e001daac052c36c09489950cd3d
JohnCampbell7 <johnc7910@gmail.com>
2019-10-25 16:19:06 +0100

    Dice game project first upload

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,22 +15,29 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Who           What    Value
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        
+      
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        //leftDiceNumber = 1
+        print("leftDiceNumber at beginning = \(leftDiceNumber)")
+        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][leftDiceNumber]
         
-        diceImageView1.image = #imageLiteral(resourceName: "DiceThree")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFive")
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][rightDiceNumber]
         
-        print("Button got tapped.")
+        leftDiceNumber = leftDiceNumber + 1
+        //leftDiceNumber = 2
+       print("leftDiceNumber at the end = \(leftDiceNumber)")
+        
+        rightDiceNumber = rightDiceNumber - 1
+        
     }
     
 }
