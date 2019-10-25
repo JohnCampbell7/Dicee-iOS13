373a2c044571a6f8c63418dbc193f0bae09d8fca
JohnCampbell7 <johnc7910@gmail.com>
2019-10-25 14:55:24 +0100

    Dice Game project first upload

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //IBoutlet allows me to reference an IU element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Who           What    Value
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.5
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceThree")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFive")
+        
+        print("Button got tapped.")
+    }
+    
 }
 
